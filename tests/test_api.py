427641d5ccae58b3940d1347b55ed661934a3e58
Hampapur Ajay <hajay@juniper.net>
2015-10-08 11:56:51 -0700

    Update readme and beginnings of tests

---
+++ tests/test_api.py
@@ -0,0 +1,20 @@
+from testtools import TestCase
+
+class TestApi(TestCase):
+    def test_create(self):
+        pass
+
+    def test_read(self):
+        pass
+
+    def test_update(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_id_to_fq_name(self):
+        pass
+
+    def test_fq_name_to_id(self):
+        pass
